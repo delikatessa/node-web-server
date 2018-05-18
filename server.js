2530500811948d078f8e7532e7dbaf7840fc5c5f
tanyasan <ts@theventury.com>
2018-05-18 14:43:36 +0200

    middleware + pug

--- server.js
+++ server.js
@@ -1,9 +1,57 @@
+require('dotenv').load();
+
 const express = require('express');
+const pug = require('pug');
+const fs = require('fs');
 
 var app = express();
 
-app.get('/', (req, res) => {
-	//res.send("<h1>hello</h1>");
+app.set('view engine', 'pug');
+
+app.use((req, res, next) => {
+	const now = new Date().toString();
+	const log = `${now}: ${req.method} ${req.url}`;
+	console.log(log);
+	fs.appendFile('server.log', log + '\n', err => {
+		if (err) {
+			console.log('Unable to append server.log');
+		}
+	});
+	next();
 });
 
-app.listen(3000);
+app.use((req, res, next) => {
+	if (process.env.MAINTENANCE === true) {
+		res.render('maintenance', {
+			pageTitle: 'Maintenance mode',
+			message: 'We will be back soon',
+		});
+	} else {
+		next();
+	}
+});
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => {
+	res.render('home', {
+		pageTitle: 'Home',
+		welcomeMessage: 'Welcome!',
+	});
+});
+
+app.get('/about', (req, res) => {
+	res.render('about', {
+		pageTitle: 'About',
+	});
+});
+
+app.get('/bad', (req, res) => {
+	res.send({
+		errorMessage: 'Unable to handle request',
+	});
+});
+
+app.listen(3000, () => {
+	console.log('Server is up on port 3000');
+});
